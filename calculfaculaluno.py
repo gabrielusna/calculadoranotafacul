c80710e001adfd83bd6026d089a8307b451c92bb
Gabriel <91698298+gabrielusna@users.noreply.github.com>
2021-11-13 22:40:41 -0300

    Update calculfaculaluno.py

--- calculfaculaluno.py
+++ calculfaculaluno.py
@@ -4,30 +4,30 @@ reprovados = []
 while True:
   aluno = str(input("Qual é seu nome ?: "))
   while True: 
-    ativ1 = float(input("NOTA [0,1] Atividade Online Pontuada 1: "))
+    ativ1 = float(input(f"{aluno}, Qual Sua Nota Da Atividade Um ?: "))
     if ativ1  < 0.0 or ativ1 > 1.0:
-      print("Nota Incorreta, Tente Novamente!")
+      print("Sua Nota Está Incorreta, Tente Novamente!")
     else: 
       break
 
   while True:
-    ativ2 = float(input("NOTA [0,2] Atividade Online Pontuada 2: "))
+    ativ2 = float(input(f"{aluno}, Qual Sua Nota Da Atividade Dois ?: "))
     if ativ2  < 0.0 or ativ2 > 2.0:
-      print("Nota Incorreta, Tente Novamente!")
+      print("Sua Nota Está Incorreta, Tente Novamente!")
     else: 
       break
 
   while True:
-    ativ3 = float(input("NOTA [0,1] Atividade Online Pontuada 3: "))
+    ativ3 = float(input(f"{aluno}, Qual Sua Nota Da Atividade Três ?: "))
     if ativ3  < 0.0 or ativ3 > 1.0:
-      print("Nota Incorreta, Tente Novamente!")
+      print("Sua Nota Está Incorreta, Tente Novamente!")
     else: 
       break
 
   while True:
-    provaregular = float(input("NOTA [0,6] Prova Regular: "))
+    provaregular = float(input(f"{aluno}, Qual Sua Nota Da Prova Regular ?: "))
     if provaregular  < 0.0 or provaregular > 6.0:
-      print("Nota Incorreta, Tente Novamente!")
+      print("Sua Nota Está Incorreta, Tente Novamente!")
     else: 
       break
   media = (ativ1 + ativ2 + ativ3 + provaregular) 
@@ -35,16 +35,22 @@ while True:
     print(f"O aluno {aluno} foi APROVADO com {media} pontos")
     aprovados.append(aluno)
   if (media <=6):
-    print(f"O aluno {aluno} ficou com {media} pontos, e não atingiu a média!, Agora precisa fazer a recuperação!") 
-    recuperacao = float(input("Nota recuperação: "))
-    if recuperacao >= 5.0:
-      print(f"O aluno {aluno} ficou com {recuperacao} pontos na recuperação, e foi APROVADO!") 
-      aprovados.append(aluno)
-    else:
-      print(f"O aluno {aluno} ficou com {recuperacao} pontos na recuperação, e foi REPROVADO!")
-      reprovados.append(aluno)
+    print(f"O aluno {aluno} ficou com {media} pontos, e não atingiu a média! \nAgora precisa fazer a recuperação!") 
+    while True:
+      recuperacao = float(input(f"Digite Sua Nota De Recuperação, {aluno}: "))
+      if recuperacao >= 5.0 and recuperacao <= 10.0:
+        print(f"O aluno {aluno} ficou com {recuperacao} pontos na recuperação, e foi APROVADO!") 
+        aprovados.append(aluno)
+        print("-" * 48)
+        break
+      elif recuperacao >= 0 and recuperacao < 5:
+        print(f"O aluno {aluno} ficou com {recuperacao} pontos na recuperação, e foi REPROVADO!")
+        reprovados.append(aluno)
+        print("-" * 48)
+        break
+      else:
+        print("Sua Nota Está Incorreta, Tente Novamente!")
 
-      print("-" * 48)
   while True:
    continuar = str(input("Gostaria de calcular mais uma nota? [s/n] "))
    if continuar == "s":                                                              
@@ -60,4 +66,3 @@ aprovados_pcento = (100 / total_alunos) * len(aprovados)
 reprovados_pcento = (100 / total_alunos) * len(reprovados)
 
 print("{:.2f}% alunos foram APROVADOS e {:.2f}% ficaram REPROVADOS".format(aprovados_pcento, reprovados_pcento))
- 
